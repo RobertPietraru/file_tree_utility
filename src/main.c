ad67f3516c0ed83c5fc728ef88ae46b1f08a8496
RobertPietraru <rob_piet@yahoo.com>
2023-12-23 22:52:36 +0200

    dir name | spaces

--- src/main.c
+++ src/main.c
@@ -9,143 +9,158 @@
 
 int show_hidden_files = 0;
 void get_target_directory(char *cwd, char *target_path) {
-	int target_path_length = 0;
+  int target_path_length = 0;
 
-	while (target_path[target_path_length] != '\0')
-		target_path_length++;
+  while (target_path[target_path_length] != '\0')
+    target_path_length++;
 
-	if (target_path_length == 0)
-		return;
+  if (target_path_length == 0)
+    return;
 
-	if (target_path[0] == '/') {
-		strcpy(cwd, target_path);
-		return;
-	}
+  if (target_path[0] == '/') {
+    strcpy(cwd, target_path);
+    return;
+  }
 
-	if (!(target_path_length == 1 && target_path[0] == '.')) {
-		strcat(cwd, "/");
-		strcat(cwd, target_path);
-	}
+  if (!(target_path_length == 1 && target_path[0] == '.')) {
+    strcat(cwd, "/");
+    strcat(cwd, target_path);
+  }
 }
 
 int is_folder_navigation_thingy(char *file) {
-	int length = strlen(file);
-	if (length == 1 && file[0] == '.')
-		return 1;
-	if (length == 2 && file[0] == '.' && file[1] == '.')
-		return 1;
+  int length = strlen(file);
+  if (length == 1 && file[0] == '.')
+    return 1;
+  if (length == 2 && file[0] == '.' && file[1] == '.')
+    return 1;
 
-	return 0;
+  return 0;
+}
+
+int get_directory_name_index(char *folder_path) {
+  int last_slash = -1;
+  int i = 0;
+  while (folder_path[i] != '\0') {
+    if (folder_path[i] == '/')
+      last_slash = i;
+    i++;
+  }
+  return last_slash + 1;
 }
 
 void show_structure(char *cwd, char *leading, int depth, int max_depth) {
-	if (depth > max_depth)
-		return;
+  if (depth > max_depth)
+    return;
 
-	char *next_leading = (char *)malloc(max_depth * 5 + 1);
-	char *next_leading_without_bar = (char *)malloc(max_depth * 5 + 1);
+  char *next_leading = (char *)malloc(max_depth * 5 + 1);
+  char *next_leading_without_bar = (char *)malloc(max_depth * 5 + 1);
 
-	// if (depth != 0) {
-	strcpy(next_leading, leading);
-	strcpy(next_leading_without_bar, leading);
+  // if (depth != 0) {
+  strcpy(next_leading, leading);
+  strcpy(next_leading_without_bar, leading);
 
-	strcat(next_leading, "| ");
-	strcat(next_leading_without_bar, "  ");
-	// }
+  strcat(next_leading, "| ");
+  strcat(next_leading_without_bar, "  ");
+  // }
 
-	DIR *dir = opendir(cwd);
+  DIR *dir = opendir(cwd);
 
-	if (dir) {
-		/* Directory exists. */
+  if (dir) {
+    /* Directory exists. */
 
-		struct stat file_stat;
+    struct stat file_stat;
 
-		struct dirent *next_entry;
-		struct dirent *entry;
+    struct dirent *next_entry;
+    struct dirent *entry;
 
-		while ((entry = readdir(dir)) != NULL) {
-			if (!is_folder_navigation_thingy(entry->d_name))
-				break;
-		}
+    while ((entry = readdir(dir)) != NULL) {
+      if (!is_folder_navigation_thingy(entry->d_name))
+        break;
+    }
 
-		if (entry == NULL) {
-			free(next_leading);
-			free(next_leading_without_bar);
-			return;
-		}
+    if (entry == NULL) {
+      free(next_leading);
+      free(next_leading_without_bar);
+      return;
+    }
 
-		char full_path[PATH_MAX];
-		while ((next_entry = readdir(dir)) != NULL) {
-			// Ignore "." and ".." entries
-			if (is_folder_navigation_thingy(next_entry->d_name))
-				continue;
-			if (!show_hidden_files && next_entry->d_name[0] == '.')
-				continue;
+    char full_path[PATH_MAX];
+    while ((next_entry = readdir(dir)) != NULL) {
+      // Ignore "." and ".." entries
+      if (is_folder_navigation_thingy(next_entry->d_name))
+        continue;
+      if (!show_hidden_files && next_entry->d_name[0] == '.')
+        continue;
 
-			snprintf(full_path, sizeof(full_path), "%s/%s", cwd, entry->d_name);
+      snprintf(full_path, sizeof(full_path), "%s/%s", cwd, entry->d_name);
 
-			printf("%s", leading);
-			printf("├");
+      printf("%s", leading);
+      printf("├");
 
-			printf("─ %s", entry->d_name);
-			if (stat(full_path, &file_stat) == 0 &&
-				S_ISDIR(file_stat.st_mode)) {
-				printf("/\n");
-				show_structure(full_path, next_leading, depth + 1, max_depth);
-			} else {
-				printf("\n");
-			}
+      printf("─ %s", entry->d_name);
+      if (stat(full_path, &file_stat) == 0 && S_ISDIR(file_stat.st_mode)) {
+        printf("/\n");
+        show_structure(full_path, next_leading, depth + 1, max_depth);
+      } else {
+        printf("\n");
+      }
 
-			entry = next_entry;
-		}
+      entry = next_entry;
+    }
 
-		snprintf(full_path, sizeof(full_path), "%s/%s", cwd, entry->d_name);
+    snprintf(full_path, sizeof(full_path), "%s/%s", cwd, entry->d_name);
 
-		printf("%s", leading);
-		printf("└");
+    printf("%s", leading);
+    printf("└");
 
-		if (stat(full_path, &file_stat) == 0 && S_ISDIR(file_stat.st_mode)) {
-			printf("─ %s/\n", entry->d_name);
-			show_structure(full_path, next_leading_without_bar, depth + 1,
-						   max_depth);
-		} else {
-			printf("─ %s\n", entry->d_name);
-		}
+    if (stat(full_path, &file_stat) == 0 && S_ISDIR(file_stat.st_mode)) {
+      printf("─ %s/\n", entry->d_name);
+      show_structure(full_path, next_leading_without_bar, depth + 1, max_depth);
+    } else {
+      printf("─ %s\n", entry->d_name);
+    }
 
-		closedir(dir);
-	} else if (ENOENT == errno) {
-		/* Directory does not exist. */
-		printf("Mate, the directory you gave me does not exist.\n");
-		exit(EXIT_FAILURE);
-	} else {
-		printf("Something went wrong. \nidk\n");
-		/* opendir() failed for some other reason. */
-	}
+    closedir(dir);
+  } else if (ENOENT == errno) {
+    /* Directory does not exist. */
+    printf("Mate, the directory you gave me does not exist.\n");
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Something went wrong. \nidk\n");
+    /* opendir() failed for some other reason. */
+  }
 
-	free(next_leading);
-	free(next_leading_without_bar);
+  free(next_leading);
+  free(next_leading_without_bar);
 }
 
 /// first parameter is the path where we show the structure
 int main(int argc, char *argv[]) {
-	char cwd[PATH_MAX];
+  char cwd[PATH_MAX];
 
-	char *x = getcwd(cwd, sizeof(cwd));
-	for (int index = 1; index < argc; index++) {
-		if (argv[index][0] != '-') {
-			get_target_directory(cwd, argv[index]);
-		}
+  char *x = getcwd(cwd, sizeof(cwd));
+  for (int index = 1; index < argc; index++) {
+    if (argv[index][0] != '-') {
+      get_target_directory(cwd, argv[index]);
+    }
 
-		if (!strcmp(argv[index], "-a")) {
-			show_hidden_files = 1;
-		}
-	}
+    if (!strcmp(argv[index], "-a")) {
+      show_hidden_files = 1;
+    }
+  }
 
-	printf("Showing file structure for %s\n\n", cwd);
+  char *dir = (char *)malloc(strlen(cwd) + 1);
+  strcpy(dir, cwd);
+  int index = get_directory_name_index(dir);
+  dir += index;
+  printf("%s\n", dir);
+  dir -= index;
+  free(dir);
 
-	show_structure(cwd, "", 0, 10);
+  show_structure(cwd, "  ", 0, 10);
 
-	/// current working directory
+  /// current working directory
 
-	return 0;
+  return 0;
 }
